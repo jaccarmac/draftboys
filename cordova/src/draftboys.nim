353cfda64bed40480113438272a5b147c5441093
jaccarmac@gmail.com <jaccarmac@gmail.com>
2015-04-01 21:06:56 +0000

    Add App Engine project and API call.
    FossilOrigin-Name: 96942470ebe675933bd5176444930f4a3d075da9

---
+++ cordova/src/draftboys.nim
@@ -0,0 +1,16 @@
+import dom
+
+const
+  endpoint = "http://0-dot-draftboys-dota.appspot.com"
+
+proc lazyRequest(endpoint: cstring): cstring {.importc.}
+
+proc placeholderListener(event: ref TEvent) =
+  let
+    placeholder = document.getElementById("placeholder")
+    api = lazyRequest(endpoint)
+  placeholder.innerHTML = "Hello from Nim! "
+  placeholder.innerHTML = $placeholder.innerHTML & $api
+
+
+document.addEventListener("deviceready", placeholderListener)
--- src/draftboys.nim
+++
@@ -1,6 +0,0 @@
-import dom
-
-proc placeholderListener(event: ref TEvent) =
-  document.getElementById("placeholder").innerHTML = "Hello from Nim!"
-
-document.addEventListener("deviceready", placeholderListener)
